c926b838f1afb52097b4fa5fdf2dffddf1e96344
smothers15 <dylo@fresnou.org>
2021-11-05 09:50:49 -0700

    todo models

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,11 +1,12 @@
 const {sequelize} = require("../models/index");
 const {QueryTypes} = require("sequelize");
+const {Todo} = require('../models/index');
 
 
 module.exports.homeRoute = async function(req, res, next) {
 
 
-    let toDoItems = await sequelize.query('select * from todo', {type: QueryTypes.SELECT});
+    let toDoItems = await Todo.findAll();
     res.render('index', {toDoItems});
 };
 
@@ -15,20 +16,16 @@ module.exports.renderAddForm = function(req, res, next){
 
 module.exports.addNewItem = async function(req, res, next){
 
-    await sequelize.query('insert into todo (description) values (:description)', {
-        type: QueryTypes.INSERT,
-        replacements: {
-            description: req.body.description
-        }
+    await Todo.create({
+        description: req.body.description
     });
     res.redirect('/');
 };
 
 module.exports.markItemAsComplete = async function(req, res) {
 
-    await sequelize.query('update todo set completed = true where id = :id',{
-        type: QueryTypes.UPDATE,
-        replacements: {
+    await Todo.update({completed: true }, {
+        where: {
             id: req.params.id
         }
     });
@@ -37,9 +34,8 @@ module.exports.markItemAsComplete = async function(req, res) {
 
 module.exports.markItemAsIncomplete = async function(req, res) {
 
-    await sequelize.query('update todo set completed = false where id = :id',{
-        type: QueryTypes.UPDATE,
-        replacements: {
+    await Todo.update({completed: false }, {
+        where: {
             id: req.params.id
         }
     });
@@ -48,9 +44,8 @@ module.exports.markItemAsIncomplete = async function(req, res) {
 
 module.exports.deleteItem = async function(req, res) {
 
-    await sequelize.query('delete from todo where id = :id',{
-        type: QueryTypes.DELETE,
-        replacements: {
+    await Todo.destroy ({
+        where: {
             id: req.params.id
         }
     });
@@ -59,24 +54,20 @@ module.exports.deleteItem = async function(req, res) {
 
 module.exports.renderEditForm = async function(req, res) {
 
-    const results = await sequelize.query('select * from todo where id=:id',{
-        type: QueryTypes.SELECT,
-        replacements: {
-            id: req.params.id
+    let todo = await Todo.findByPk(req.params.id);
+    res.render('edit_todo', {
+        item: {
+            description: todo.description,
+            id: todo.id
         }
     });
-    const item = results[0];
-    //console.log(results);
-    res.render('edit_todo', {item})
 };
 
 module.exports.updateItem = async function(req, res) {
 
-    await sequelize.query('update todo set description = :description where id = :id',{
-        type: QueryTypes.UPDATE,
-        replacements: {
-            id: req.params.id,
-            description: req.body.description
+    await Todo.update({description: req.body.description}, {
+        where: {
+            id: req.params.id
         }
     });
     res.redirect('/');
